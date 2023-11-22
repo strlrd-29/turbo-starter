75750e07656c64b34676a320e70d2e72fb2a2e6a
Ouassim Abdelmalek Ghribi <oag@big-mama.io>
2023-11-22 22:15:33 +0100

    style: added prettier config and formatted code with new config

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,16 +1,16 @@
-import Image from "next/image";
-import { Card } from "@repo/ui/card";
-import { Code } from "@repo/ui/code";
-import styles from "./page.module.css";
+import Image from 'next/image'
+import { Card } from '@repo/ui/card'
+import { Code } from '@repo/ui/code'
+import styles from './page.module.css'
 
 function Gradient({
   conic,
   className,
-  small,
+  small
 }: {
-  small?: boolean;
-  conic?: boolean;
-  className?: string;
+  small?: boolean
+  conic?: boolean
+  className?: string
 }): JSX.Element {
   return (
     <span
@@ -18,37 +18,36 @@ function Gradient({
         styles.gradient,
         conic ? styles.glowConic : undefined,
         small ? styles.gradientSmall : styles.gradientLarge,
-        className,
+        className
       ]
         .filter(Boolean)
-        .join(" ")}
+        .join(' ')}
     />
-  );
+  )
 }
 
 const LINKS = [
   {
-    title: "Docs",
-    href: "https://turbo.build/repo/docs",
-    description: "Find in-depth information about Turborepo features and API.",
+    title: 'Docs',
+    href: 'https://turbo.build/repo/docs',
+    description: 'Find in-depth information about Turborepo features and API.'
   },
   {
-    title: "Learn",
-    href: "https://turbo.build/repo/docs/handbook",
-    description: "Learn more about monorepos with our handbook.",
+    title: 'Learn',
+    href: 'https://turbo.build/repo/docs/handbook',
+    description: 'Learn more about monorepos with our handbook.'
   },
   {
-    title: "Templates",
-    href: "https://turbo.build/repo/docs/getting-started/from-example",
-    description: "Choose from over 15 examples and deploy with a single click.",
+    title: 'Templates',
+    href: 'https://turbo.build/repo/docs/getting-started/from-example',
+    description: 'Choose from over 15 examples and deploy with a single click.'
   },
   {
-    title: "Deploy",
-    href: "https://vercel.com/new",
-    description:
-      " Instantly deploy your Turborepo to a shareable URL with Vercel.",
-  },
-];
+    title: 'Deploy',
+    href: 'https://vercel.com/new',
+    description: ' Instantly deploy your Turborepo to a shareable URL with Vercel.'
+  }
+]
 
 export default function Page(): JSX.Element {
   return (
@@ -64,7 +63,7 @@ export default function Page(): JSX.Element {
             rel="noopener noreferrer"
             target="_blank"
           >
-            By{" "}
+            By{' '}
             <Image
               alt="Vercel Logo"
               className={styles.vercelLogo}
@@ -81,25 +80,14 @@ export default function Page(): JSX.Element {
         <div className={styles.heroContent}>
           <div className={styles.logos}>
             <div className={styles.circles}>
-              <Image
-                alt="Turborepo"
-                height={614}
-                src="circles.svg"
-                width={614}
-              />
+              <Image alt="Turborepo" height={614} src="circles.svg" width={614} />
             </div>
             <div className={styles.logoGradientContainer}>
               <Gradient className={styles.logoGradient} conic small />
             </div>
 
             <div className={styles.logo}>
-              <Image
-                alt=""
-                height={120}
-                priority
-                src="turborepo.svg"
-                width={120}
-              />
+              <Image alt="" height={120} priority src="turborepo.svg" width={120} />
             </div>
           </div>
           <Gradient className={styles.backgroundGradient} conic />
@@ -133,5 +121,5 @@ export default function Page(): JSX.Element {
         ))}
       </div>
     </main>
-  );
+  )
 }
---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  singleQuote: true,
+  semi: false,
+  arrowParens: 'always',
+  trailingComma: 'none',
+  printWidth: 100,
+  tabWidth: 2,
+  plugins: ['prettier-plugin-tailwindcss']
+}
